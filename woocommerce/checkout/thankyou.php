ce05b9ac9a8c7a1d47694b13f2bbbd1df6f68180
roadthemez <roadthemez@gmail.com>
2016-01-06 08:44:21 +0700

    init
    bắt đầu up lên git

---
+++ woocommerce/checkout/thankyou.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Thankyou page
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( $order ) : ?>
+<div class="checkout-done">
+	<?php if ( $order->has_status( 'failed' ) ) : ?>
+
+		<p><?php _e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction.', 'chairman' ); ?></p>
+
+		<p><?php
+			if ( is_user_logged_in() )
+				_e( 'Please attempt your purchase again or go to your account page.', 'chairman' );
+			else
+				_e( 'Please attempt your purchase again.', 'chairman' );
+		?></p>
+
+		<p>
+			<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php _e( 'Pay', 'chairman' ) ?></a>
+			<?php if ( is_user_logged_in() ) : ?>
+			<a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'myaccount' ) ) ); ?>" class="button pay"><?php _e( 'My Account', 'chairman' ); ?></a>
+			<?php endif; ?>
+		</p>
+
+	<?php else : ?>
+
+		<p><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'chairman' ), $order ); ?></p>
+
+		<ul class="order_details order_summary">
+			<li class="order">
+				<?php _e( 'Order:', 'chairman' ); ?>
+				<strong><?php echo ''.$order->get_order_number(); ?></strong>
+			</li>
+			<li class="date">
+				<?php _e( 'Date:', 'chairman' ); ?>
+				<strong><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></strong>
+			</li>
+			<li class="total">
+				<?php _e( 'Total:', 'chairman' ); ?>
+				<strong><?php echo ''.$order->get_formatted_order_total(); ?></strong>
+			</li>
+			<?php if ( $order->payment_method_title ) : ?>
+			<li class="method">
+				<?php _e( 'Payment method:', 'chairman' ); ?>
+				<strong><?php echo ''.$order->payment_method_title; ?></strong>
+			</li>
+			<?php endif; ?>
+		</ul>
+		<div class="clear"></div>
+
+	<?php endif; ?>
+
+	<?php do_action( 'woocommerce_thankyou_' . $order->payment_method, $order->id ); ?>
+	<?php do_action( 'woocommerce_thankyou', $order->id ); ?>
+
+<?php else : ?>
+
+	<p><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'chairman' ), null ); ?></p>
+</div>
+<?php endif; ?>
